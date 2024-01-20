512dc5a6ade20b3c6f367f5200d0b43c76fd77f0
Janemwaura <mwaura461@gmail.com>
2024-01-20 22:58:50 +0300

    complete

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,32 @@
 def badge_maker(name):
-    return None
+    return "Hello, my name is {}.".format(name)
+
+def generate_badges(names):
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
+
+def badge_maker(name):
+    return "Hello, my name is {}.".format(name)
 
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badges.append(badge_maker(name))
+    return badges
 
-def assign_rooms(names):
-    return None
+def assign_rooms(speakers):
+    room_assignments = []
+    for i, speaker in enumerate(speakers, start=1):
+        room_assignments.append("Hello, {}! You'll be assigned to room {}!".format(speaker, i))
+    return room_assignments
 
-def printer(names):
-    return None
+def printer(speakers):
+    badges = batch_badge_creator(speakers)
+    for badge in badges:
+        print(badge)
+    
+    room_assignments = assign_rooms(speakers)
+    for assignment in room_assignments:
+        print(assignment)
